13a919d71468dfb2811b6b082e9c551767b2e0a3
StandingPad Animations <mahidsheikh@pm.me>
2023-11-04 14:37:44 -0500

    Editor now loads all material properties

--- MCprep_addon/vivy_editor.py
+++ MCprep_addon/vivy_editor.py
@@ -2,7 +2,7 @@ import bpy
 from .conf import env
 
 class ListVivyMaterials(bpy.types.PropertyGroup):
-	material: bpy.props.StringProperty()
+	name: bpy.props.StringProperty()
 
 class VivyEditorProps(bpy.types.PropertyGroup):
 	vivy_materials: bpy.props.CollectionProperty(type=ListVivyMaterials)
@@ -32,7 +32,7 @@ class VIVY_OT_reload_editor(bpy.types.Operator):
 
 	def execute(self, context):
 		env.reload_vivy_json()
-		reload_vivy_materials(context)	
+		reload_vivy_materials(context)
 		return {'FINISHED'}
 
 class VIVY_PT_editor(bpy.types.Panel):
@@ -44,8 +44,31 @@ class VIVY_PT_editor(bpy.types.Panel):
 	def draw(self, context):
 		layout = self.layout
 		vprop = context.scene.vivy_editor_props
-		if vprop.vivy_materials:
+		if vprop.vivy_materials and env.vivy_material_json is not None:
 			layout.template_list("VIVY_UL_materials", "", vprop, "vivy_materials", vprop, "vivy_materials_index", rows=4)
+			mat = vprop.vivy_materials[vprop.vivy_materials_index]
+			mp = env.vivy_material_json["materials"][mat.name]
+			box = layout.box()
+			box.label(text=f"{mat.name} Properties")
+
+			row = box.row()
+			row.label(text=f"Base Material: {mp['base_material']}")
+			row = box.row()
+			row.label(text=mp["desc"])
+			
+			if "passes" in mp:
+				nbox = box.box()
+				nbox.label(text="Passes")
+				for p in mp["passes"]:
+					row = nbox.row()
+					row.label(text=f"{p.capitalize()}: {mp['passes'][p]}")
+			if "refinements" in mp:
+				nbox = box.box()
+				nbox.label(text="Refinements")
+				for r in mp["refinements"]:
+					row = nbox.row()
+					row.label(text=f"{r.capitalize()}: {mp['refinements'][r]}")
+				
 		else:
 			layout.operator("vivy.reload_editor")	
 
